489cc4a8795118780157427d69362e4c9d8849cb
Dojima Shinji <100776572+Xhideto@users.noreply.github.com>
2024-10-20 22:33:00 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
+const hre = require("hardhat");
 
+async function main() {
+  const Betting = await hre.ethers.getContractFactory("Betting");
+  const betting = await Betting.deploy();
+  await betting.deployed();
+
+  console.log("Betting contract deployed to:", betting.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
